bfa20e83a6a3685ae612e81fbad1bffbd7115825
IrvinA <arevalosirvin@gmail.com>
2021-09-17 02:23:12 -0700

    Home.js structure complete

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
-    
+    const history = useHistory();
+    const routeToForm = () => {
+        history.push('/pizza');
+    }
+
+    return (
+        <div>
+            <img src='../Assets/Pizza.jpg' alt='Pizza'/>
+            <h2>Order some mouth-watering pizza!<br /> 
+            You know you want to...</h2>
+            <button onClick={routeToForm}>Order it!</button>
+        </div>
+    )
 }
